8f485316f0c6b8ec23f41d02e9130038ffb5a21d
Sreedevi <sreedevi@luminartechnolab.com>
2022-09-19 11:37:13 +0530

    commit

---
+++ lib/Drawer/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:samplemay/Drawer/screens/homepage.dart';
+
+void main() => runApp(MaterialApp(
+      theme: ThemeData(primarySwatch:Colors.pink ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
